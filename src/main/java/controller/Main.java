5451805c401be53a64a932cf8801bc747dc981f6
Eduardo Castro <educcjr@gmail.com>
2015-01-30 00:23:56 -0200

    Exclude saveMp3AndBackup() inner calls

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -11,17 +11,19 @@ public class Main
     public static void main(String[] args)
     {
         /* Testes */
-        Mp3FolderController folderController = new Mp3FolderController("C:/Users/ecastro/Desktop/mp3dit tests");
+        Mp3FolderController folderController = new Mp3FolderController("C:/Users/duds410/Desktop/mp3tests");
 
         ArrayList<Mp3File> mp3FileList = folderController.getAllMp3Files();
 
         folderController.updateID3ToV23(mp3FileList);
+        folderController.saveMp3AndBackup(mp3FileList,"obsolete");
 
+        mp3FileList = folderController.getAllMp3Files();
         if (folderController.setFilenameAsTag(mp3FileList,new Reference('-'),ID3v2Tag.ALBUMARTIST) &&
             folderController.setFilenameAsTag(mp3FileList,new Reference('-',true),ID3v2Tag.TITLE))
         {
             folderController.saveMp3AndBackup(mp3FileList,"bak");
-            folderController.saveMp3(mp3FileList);
+            //folderController.saveMp3(mp3FileList);
         }
     }
 }
